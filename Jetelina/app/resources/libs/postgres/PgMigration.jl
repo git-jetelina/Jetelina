233b053ed1147e0d054660d4d2ee3de9a0482673
ono keiji <onokeiji@gmail.com>
2026-01-29 09:26:53 +0900

    first functions

--- Jetelina/app/resources/libs/postgres/PgMigration.jl
+++ Jetelina/app/resources/libs/postgres/PgMigration.jl
@@ -7,6 +7,8 @@ Description:
 	Migrate existing tables to Jetelina tables for PostgreSQL
 
 functions
+    function getTableList() collect the existing table's column data type
+    function collect_columns_data_type(String::tablename) collect the existing table's column data type
 
 """
 module PgMigration
@@ -22,20 +24,50 @@ include("PgDataTypeList.jl")
 include("PgSQLSentenceManager.jl")
 include("PgIVMController.jl")
 
-export create_jetelina_database
+export getTableList, collect_columns_data_type
 
 """
-function create_jetelina_database()
+function getTableList()
 
-	create 'jetelina' database.
-    BUT, look like LibPQ does not support this substitution string so far, therefore abandon to create 'jetelinadb' now and 
-    keep working in default 'pg_dbname'.
-    will be real someday, hopefully  ・ω・
+	collect the existing table's column data type
 
     # Arguments
-- `s:String`: 'json' -> required JSON form to return
-			'dataframe' -> required DataFrames form to return
-- return: table list in json or DataFrame
-	
+- return: table list in json or DataFrame	
 """
+function getTableList()
+    df = DataFrame()
+    conn = PgDBController.open_connection()
+    # Fixing as 'public' in schemaname. This is the protocol.
+    table_str = """select tablename from pg_tables where schemaname='public'"""
+    try
+        df = DataFrame(columntable(LibPQ.execute(conn, table_str)))
+        # do not include usertable and ivm table in the return
+        DataFrames.filter!(row -> row.tablename != "jetelina_user_table" && row.tablename ∉ Df_JsJvList[!,:jv] , df)
+
+        #===
+            Tips:
+                select tables that does not have 'jetelina_delete_flg' in it among df
+        ===#
+    catch err
+        JLog.writetoLogfile("PgMigration.getTableList() error: $err")
+        return DataFrame() # return empty DataFrame if got fail
+    finally
+        PgDBController.close_connection(conn)
+    end
+
+    return df
+end
+
+"""
+function collect_columns_data_type(String::tablename)
+
+	collect the existing table's column data type
+
+    # Arguments
+- `tablename:String`: existing table name
+- return: columns data type list in json or DataFrame	
+"""
+function collect_columns_data_type(String::tablename)
+end
+
 end
